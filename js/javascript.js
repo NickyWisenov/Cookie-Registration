f0b575dde7febf86e0d4967a6913e94acd5fe6d6
NickyWisenov <wisenov@gmail.com>
2018-11-21 04:47:22 +1000

    Finish Project

--- js/javascript.js
+++ js/javascript.js
@@ -9,6 +9,7 @@ function pollsubmit() {
                 $.cookie("intelvotes", currentVotes + 1);
             }
             window.alert("Thank you for Voting for Intel Processor Lineup");
+            window.open('./awards.html');
         }
         else if (document.getElementById("amd").checked == true) {
             if ($.cookie("amdvotes") == null) {
@@ -19,6 +20,7 @@ function pollsubmit() {
                 $.cookie("amdvotes", currentVotes + 1);
             }
             window.alert("Thank you for Voting for AMD Processor Lineup");
+            window.open('./awards.html');
         }
         else if (document.getElementById("samsung").checked == true) {
             if ($.cookie("samsungvotes") == null) {
@@ -28,7 +30,8 @@ function pollsubmit() {
                 console.log(currentVotes);
                 $.cookie("samsungvotes", currentVotes + 1);
             }
-            window.alert("Thank you for Voting for Samsung Exynos Processor Lineup")
+            window.alert("Thank you for Voting for Samsung Exynos Processor Lineup");
+            window.open('./awards.html');
         }
         else {
             window.alert("You have not selected anything, please select your favorite processor in 2018");
@@ -71,30 +74,37 @@ function validate() {
 
 
 $(document).ready(function () {
+    // In the Poll Page Update the Votes Numbers
     if ($.cookie("intelvotes") != null) {
-        $("#intel-votes").text("Intel Votes: " + $.cookie("intelvotes"));
+        $("#intel-votes").text("Intel Votes: " + $.cookie("intelvotes")); // After Nominee of Vote Poll Page
+        $("#intel-votes-td").text($.cookie("intelvotes")); // Votes Number Update in the Awards Table
     } else {
         $("#intel-votes").text("Intel Votes: " + 0);
+        $("#intel-votes-td").text(0);
     }
 
     if ($.cookie("amdvotes") != null) {
         $("#amd-votes").text("AMD Votes: " + $.cookie("amdvotes"));
+        $("#amd-votes-td").text($.cookie("amdvotes"));
     } else {
         $("#amd-votes").text("AMD Votes: " + 0);
+        $("#amd-votes-td").text(0);
     }
 
     if ($.cookie("samsungvotes") != null) {
         $("#samsung-votes").text("SAMSUNG Votes: " + $.cookie("samsungvotes"));
+        $("#samsung-votes-td").text($.cookie("samsungvotes"));
     } else {
         $("#samsung-votes").text("SAMSUNG Votes: " + 0);
+        $("#samsung-votes-td").text(0);
     }
 
+    // In the registration Page Check the Conference ID when User click Enter Key after inputing the ID
     $("input#conferenceId").on('keypress', function (e) {
         if(e.which == 13) { // If you press Enter Key
             if ($(this).val() != "") {
                 if ($.cookie($(this).val()) != null) {
                     var currentCookie = JSON.parse($.cookie($(this).val()));
-                    console.log(currentCookie);
                     $.each(currentCookie, function(setting, value) {
                         $("#registration input, textarea, select").each(function (index, element) {
                             if (element.type == 'radio') {
@@ -126,9 +136,14 @@ $(document).ready(function () {
         }
     });
 
+    // Registration Info Submit Function
     $("#registration").submit(function (e) {
         e.preventDefault();
-        if($("#workshopB").checked == true){
+        if ($("#conferenceId").val() == '' ) {
+            alert("Please Input the Conference ID");
+        } else if ($("#conferenceId").val().length < 6) {
+            alert("Conference ID should be 6 digits");
+        } else if($("#workshopB").checked == true){
             if ($("#workshopD").checked == true || $("#workshopE").checked == true || $("#workshopF").checked == true)
                     alert("The workshop Web Programming is an all day event. Selecting this workshop requires your attendance all day so Session 2 will be be available to register for");
             if ($("#workshopF").checked == true)
@@ -138,20 +153,18 @@ $(document).ready(function () {
                 if($("#workshopF").checked == false)
                         alert("If you choose Cyber-Forensics/Hacking You must also choose Introduction to Cyber-Security for Session 2");
                         return false;
-                }
-        else{
+        } else {
             setCookie('registration');
             window.open('thankyou.html');
         }
     });
-
 
 });
 
 // Cookie Setting Function
 function setCookie(formname) {
     var data = {};
-
+    // Save Registration Data in the Array
     $("#registration input, textarea, select").each(function (index, element) {
         if (element.type == 'radio') {
             if ($(element).is(":checked")) {
@@ -162,9 +175,10 @@ function setCookie(formname) {
         }
     });
 
+    // Set Cookie Expiration Time
     var date = new Date();
     var minutes = 60; // Set Cookie Expiration Time as 60 mins
     date.setTime(date.getTime() + (minutes * 60 * 1000));
-
-    $.cookie(123456, JSON.stringify(data), { expires: date });
+    
+    $.cookie($("#conferenceId").val(), JSON.stringify(data), { expires: date });
 }
